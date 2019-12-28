f514d2c90d90ff00d6bb80282533c43cb1005d56
skucherenko <kucherenkoserhiy@gmail.com>
2019-12-28 18:23:23 +0100

    fixed other tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,10 +25,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('WhereToEat-angular');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should have router-outlet tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to WhereToEat-angular!');
+    expect(compiled.querySelector('router-outlet').textContent).toBeDefined();
   }));
 });
