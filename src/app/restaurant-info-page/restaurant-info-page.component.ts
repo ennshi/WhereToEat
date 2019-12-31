17b256ffdfe2f4c93d371a617b098a1261c77674
ennshi <enn.shi@gmail.com>
2019-12-31 17:59:37 +0100

    Creation restaurant-info-page (no design yet)

--- src/app/restaurant-info-page/restaurant-info-page.component.ts
+++ src/app/restaurant-info-page/restaurant-info-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {Restaurant} from '../shared/interfaces';
+import {RestaurantsService} from '../shared/restaurants.service';
+import {ActivatedRoute, Params} from '@angular/router';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-restaurant-info-page',
   templateUrl: './restaurant-info-page.component.html',
   styleUrls: ['./restaurant-info-page.component.scss']
 })
-export class RestaurantInfoPageComponent implements OnInit {
 
-  constructor() { }
+export class RestaurantInfoPageComponent implements OnInit {
+  restaurant: Restaurant;
+  constructor(private restaurantsService: RestaurantsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.restaurantsService.getById(params['id']);
+      })
+    ).subscribe((restaurant: Restaurant) => {
+      this.restaurant = restaurant;
+      console.log(this.restaurant);
+    });
   }
 
 }
